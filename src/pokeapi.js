74486af8de79ea9a0fdbb58e8d1b8f9c434b4912
Arch <wbhaoui@gmail.com>
2024-02-10 12:34:27 +0100

    Add getPokemonById and getPokemonCount functions

--- src/pokeapi.js
+++ src/pokeapi.js
@@ -1,4 +1,4 @@
-export {getRandomPokemon};
+export {getRandomPokemon, getPokemonCount, getPokemonById};
 
 const getRandomPokemon = async () => {
   const count_response = await fetch("https://pokeapi.co/api/v2/pokemon-species?limit=1")
@@ -6,4 +6,16 @@ const getRandomPokemon = async () => {
   const id = Math.floor(Math.random()*parseInt(count_json.count)) + 1;
   const pokemon_response =  await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
   return pokemon_response.json()
+}
+
+const getPokemonCount = async () => {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon-species?limit=1");
+  const json = await response.json();
+  return json.count;
+}
+
+const getPokemonById = async (id) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const json = await response.json();
+  return json;
 }
