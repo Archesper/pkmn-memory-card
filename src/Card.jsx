6c62e0b4d8f58d284cb7403ea393d56bcba31299
Arch <wbhaoui@gmail.com>
2024-02-10 12:35:40 +0100

    Add card component

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,13 @@
-export default function Card() {
+import './styles/card.css'
+
+export default function Card({title, image, titleRef, imgRef}) {
   return (
-    <div className="card"></div>
+    <div className="card">
+      <div className="cardFront">
+        <img className="sprite" ref={imgRef} src={image}></img>
+        <p className="card-title" ref={titleRef}>{title}</p>
+      </div>
+      <div className="cardBack"><div className="card-ball"></div></div>
+    </div>
   )
 }
