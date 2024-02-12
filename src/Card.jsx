d82610f0701f46c113bdccb49bcd23421c0b874a
Arch <wbhaoui@gmail.com>
2024-02-12 18:06:15 +0100

    Add pokeball graphic

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,13 +1,33 @@
-import './styles/card.css'
+import "./styles/card.css";
 
-export default function Card({id, title, image, titleRef, imgRef, onClick}) {
+export default function Card({
+  id,
+  title,
+  image,
+  titleRef,
+  imgRef,
+  onClick,
+  flipped,
+}) {
+  const classes = "card" + (flipped ? " flipped" : "");
   return (
-    <div data-id={id} onClick={onClick} className="card">
+    <div data-id={id} onClick={onClick} className={classes}>
       <div className="cardFront">
         <img className="sprite" ref={imgRef} src={image}></img>
-        <p className="card-title" ref={titleRef}>{title}</p>
+        <p className="card-title" ref={titleRef}>
+          {title}
+        </p>
       </div>
-      <div className="cardBack"><div className="card-ball"></div></div>
+      <div className="cardBack">
+        <div className="card-ball">
+          <div className="ball-center">
+            <div className="inner-ring"></div>
+          </div>
+          <div className="ball-strikethrough"></div>
+          <div className="ball-lower-half"></div>
+        </div>
+
+      </div>
     </div>
-  )
+  );
 }
